9a62b0b07de6413f0a6bd21c417b95a1534526c5
Sarah Smith <sarahsmithvii@gmail.com>
2023-06-07 11:17:00 +0100

    added react components

---
+++ pages/addjob.tsx
@@ -0,0 +1,7 @@
+const addjob = () => {
+    return (
+        <h2>Add job</h2>
+      );
+}
+ 
+export default addjob;
---
+++ pages/folder.tsx
@@ -0,0 +1,7 @@
+const addfolder = () => {
+    return ( 
+        <h2>add folder</h2>
+     );
+}
+ 
+export default addfolder;
