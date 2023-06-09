1ce82d51eca18539dddb0d1a1ef6fdf3afce50d9
Sarah Smith <sarahsmithvii@gmail.com>
2023-06-09 10:42:03 +0100

    testing tab functionality

--- pages/myjobs.tsx
+++ pages/myjobs.tsx
@@ -1,16 +1,22 @@
-import Tab from "@/components/molecules/tab";
-import Tabs from "@/components/organisms/tabs";
+
+import MyTabs from "@/components/organisms/mytabs";
 
 const MyJobs = () => {
     return ( 
         <>
-        <h1>Here are my jobs, woo!</h1>
-        <Tabs>
+        <h1 className='bg-jaws-blue'>Here are my jobs, woo!</h1>
+        {/* <Tabs>
+
             <Tab title="apply">Job 1</Tab>
+
             <Tab title="applied">Job 2</Tab>
             <Tab title="interview">Job 3</Tab>
             <Tab title="result">Job 4</Tab>
-        </Tabs>
+
+        </Tabs> */}
+        <div className='bg-jaws-blue text-jaws-blue h-screen'>
+            <MyTabs/>
+        </div>
 
 
         </>
