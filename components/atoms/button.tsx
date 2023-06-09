9462d4b3e8d9bd53d77e2452ebb828efb4d04ea4
jasperBailey <jasperbailey98@gmail.com>
2023-06-09 17:05:20 +0100

    stuff

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -1,11 +1,12 @@
 interface buttonProps {
     buttonText: String;
+    type: "button"|"submit";
 }
 
-const Button:React.FC<buttonProps> = ({buttonText}) => {
+const Button:React.FC<buttonProps> = ({buttonText, type="button"}) => {
     return ( 
         <>
-        <button className="bg-jaws-blue text-jaws-white rounded-lg hover:bg-jaws-light-blue">{buttonText}</button>
+        <button type={type} className="bg-jaws-blue text-jaws-white rounded-lg hover:bg-jaws-light-blue">{buttonText}</button>
         </>
      );
 }
--- pages/addjob.tsx
+++ pages/addjob.tsx
@@ -1,13 +1,35 @@
+import Button from "@/components/atoms/button";
 import Title from "@/components/atoms/title";
-import MenuSelect from "@/components/molecules/menuselect";
 import PageTemplate from "@/components/templates/pagetemplate";
 
 const addjob = () => {
+    const handleSubmit = async (evt:any) => {
+        evt.preventDefault();
+        const response = await fetch(
+            "https://jobshark.bravepond-dfd63b32.uksouth.azurecontainerapps.io/jobs",
+
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Basic ${btoa('71847e48-8f72-4b7b-ae2d-71d746d1ca25:')}`
+                },
+                body: JSON.stringify({
+                    user_id: 1
+                }),
+            }
+        );
+        console.log(response);
+    };
+
     return (
         <PageTemplate>
-            <Title text="Add a job listing"/>
+            <Title text="Add a job listing" />
+            <form onSubmit={handleSubmit}>
+                <Button type="submit" buttonText="Add Job"/>
+            </form>
         </PageTemplate>
-      );
-}
- 
+    );
+};
+
 export default addjob;
