9df585f9fbadcb52b4a1496b9355cd5a8171b080
Sarah Smith <sarahsmithvii@gmail.com>
2023-06-09 14:10:31 +0100

    updated route to profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,13 @@
+import PageTemplate from "@/components/templates/pagetemplate";
 import Link from "next/link";
 
 
 const profile: React.FC = () => {
     return (  
         <>
+        <PageTemplate>
         <h1>Profile</h1>
-
+        </PageTemplate>
         </>
     );
 }
