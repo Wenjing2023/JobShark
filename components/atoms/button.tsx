55d6e892cadea9223c70af98a9c4fdef6eddce83
Wenjing2023 <wenjing2018uk@gmail.com>
2023-06-11 15:39:45 +0100

    button onclick removed

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -1,18 +1,15 @@
 interface buttonProps {
-    buttonText: String;
-    onClick: () => void;
+  buttonText: String;
 }
 
-const Button:React.FC<buttonProps> = ({buttonText, onClick}) => {
-    return ( 
-        <>
-        <button 
-        onClick={onClick}
-        className="bg-jaws-blue text-jaws-white rounded-lg hover:bg-jaws-light-blue p-2 px-5">{buttonText}</button>
-        </>
+const Button: React.FC<buttonProps> = ({ buttonText }) => {
+  return (
+    <>
+      <button className="bg-jaws-blue text-jaws-white rounded-lg hover:bg-jaws-light-blue p-2 px-5">
+        {buttonText}
+      </button>
+    </>
+  );
+};
 
-        
-     );
-}
- 
 export default Button;
