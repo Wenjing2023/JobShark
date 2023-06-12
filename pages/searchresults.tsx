ac7e8407d2fbd32b74ebba380baaaa60d439cb89
Wenjing2023 <wenjing2018uk@gmail.com>
2023-06-12 14:49:22 +0100

    reedsearch started

--- pages/searchresults.tsx
+++ pages/searchresults.tsx
@@ -3,26 +3,7 @@ import { useState, useEffect } from 'react';
 import SearchedJob from "@/components/molecules/searchedjob";
 import { loadAllJobs } from './api/reedapi';
 
-interface SearchedJob{
-    job: {
-			jobId: number,
-			employerId: number,
-			employerName: string,
-			jobTitle: string,
-			locationName: string,
-			minimumSalary: number,
-			maximumSalary: number,
-			currency: string,
-			expirationDate: number,
-			date: Date,
-			jobDescription: string,
-			applications: number,
-			jobUrl: string
-		}
-}
-
-
-const SearchResults:React.FC<SearchedJob> = ({job}) => {
+const SearchResults:React.FC = ({}) => {
     const [allJobs, setAllJobs] = useState<[]>([]);
     const [loading, setLoading] = useState(true);
 
