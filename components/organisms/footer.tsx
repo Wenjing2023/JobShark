9e6e3cc2acc3244e6316b057dd1edf442c72619a
Wenjing2023 <wenjing2018uk@gmail.com>
2023-06-12 09:51:13 +0100

    adding a job started

--- components/organisms/footer.tsx
+++ components/organisms/footer.tsx
@@ -1,11 +1,8 @@
-
-
-
 const Footer = () => {
   return (
     <>
       <footer>
-        <div className="bg-jaws-white p-7 text-center h-12 ">
+        <div className="bg-jaws-white p-7 text-center h-12 footer-container fixed bottom-0 left-0 w-full py-4 md:py-2">
           <span>© 2023 Copyright: JAWS</span>
         </div>
       </footer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,12 @@
 import * as React from "react";
 import PageTemplate from "../components/templates/pagetemplate";
 import Schedule from "../components/organisms/schedule";
-import Button from "../components/atoms/button";
 import dynamic from "next/dynamic";
-import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import MyJobs from "./myjobs";
 import Title from "@/components/atoms/title";
 
-const Toggle = dynamic(() => import("../components/atoms/toggle"), {
-  ssr: false,
-});
 
-interface User {
-  name: string;
-}
 
 const HomePage = () => {
   const { user, error, isLoading } = useUser();
@@ -26,8 +17,6 @@ const HomePage = () => {
     setIsLoggedIn(!!user);
   }, [user]);
 
-
-
   console.log(user);
 
   return (
@@ -36,11 +25,12 @@ const HomePage = () => {
         <div className="container mx-auto"></div>
         {isLoggedIn ? (
           <>
-            <h3 className="text-center text-jaws-blue">Welcome to JobShark, {user?.name}</h3>
-         
+            <h3 className="text-center text-jaws-blue">
+              Welcome to JobShark, {user?.name}
+            </h3>
           </>
         ) : (
-          <a href="/api/auth/login">Login</a>
+          <a href="/api/auth/login"></a>
         )}
         <Title text="My Schedule" />
         <Schedule />
