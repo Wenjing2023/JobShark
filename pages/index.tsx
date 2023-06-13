6a44fc8e6ddfa27c992d5ed40514b8bdb8456f9e
Wenjing2023 <wenjing2018uk@gmail.com>
2023-06-13 21:58:44 +0100

    more css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,33 +6,37 @@ import dynamic from "next/dynamic";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Title from "@/components/atoms/title";
-import { useState, useEffect } from 'react';
-
-
+import { useState, useEffect, useRef } from 'react';
 
 
 const HomePage = () => {
   const { user, error, isLoading } = useUser();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const hideDivRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setIsLoggedIn(!!user);
     console.log(user);
+    const timeoutId = setTimeout(() => {
+      if (hideDivRef.current) {
+        hideDivRef.current.style.opacity = "0";
+        hideDivRef.current.style.transition = "opacity 1.5s";
+      }
+    }, 2000);
+    return () => clearTimeout(timeoutId);
   }, [user]);
 
+const hideText=()=>{
 
+}
   
   return (
     <>
       <PageTemplate>
-        <div className="container mx-auto"></div>
         {isLoggedIn ? (
-          <>
-            <h3 className="text-center text-jaws-blue">
-              Welcome to JobShark, {user?.name}
-              
+            <h3 className="text-center text-jaws-blue font-bold" ref={hideDivRef} mx-auto >
+              Welcome to JobShark, {user?.name} 
             </h3>
-          </>
         ) : (
           <a href="/api/auth/login"></a>
         )}
